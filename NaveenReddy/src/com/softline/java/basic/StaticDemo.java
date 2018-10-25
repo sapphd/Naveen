34383ba761a2d4fc53deb3cc9dcf055a2806e7fb
sapphd <melihnizamoglu@yahoo.com>
2018-10-25 11:12:05 -0600

    test

---
+++ NaveenReddy/src/com/softline/java/basic/StaticDemo.java
@@ -0,0 +1,35 @@
+package com.softline.java.basic;
+
+class Emp{
+	int eid ; 
+	int salary ; 
+	static String ceo ;
+	
+	static {
+		ceo = "Veli"; 
+	}
+
+	@Override
+	public String toString() {
+		return "Emp [eid=" + eid + ", salary=" + salary + " Ceo =" + ceo + "]";
+	}
+	
+}
+
+public class StaticDemo {
+	
+    public static void main (String[] args) {
+    
+    	Emp emp1 = new Emp() ;
+    	emp1.eid = 6 ; 
+    	emp1.salary=4000 ; 
+    	emp1.ceo = "Ali" ; 
+    	 
+    	
+    	System.out.println(emp1);
+    }
+	 
+	
+	
+
+}
